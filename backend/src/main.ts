e99545dbb809c852c88548ea63a2ae9a91291c6e
Anthony Pierre Aflatoun <anthony_pierre.aflatoun@smail.th-koeln.de>
2023-11-27 23:39:12 +0100

    init repo config: Monorepo + nest & next

---
+++ backend/src/main.ts
@@ -0,0 +1,15 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const configService = app.get(ConfigService);
+
+  const port: number = configService.get('NEST_PORT', 8000);
+  await app.listen(port);
+
+  console.log(`Application is running on: http://localhost:` + port);
+}
+bootstrap();
